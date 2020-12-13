db3c5ac709e7417a165e126d1c92e1428f6801ac
Tony Watkins <tony@llifion.net>
2020-12-13 17:58:05 +0000

    To avoid running into the context error with "this", make the on search submit function and arrow function. Usefully this also shows how to make an arrow function async and have it await the response

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,24 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     
-    async onSearchSubmit(term) {
+    state = { images: [] };
+    
+    onSearchSubmit = async (term) => { 
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
-                Authorization: 'Client-ID UNSPLASH_ACCESS_KEY'
+                Authorization: 'Client-ID UNSPLASH_ACCESS_CODE'
             }
         });
         
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
     
     render () {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
