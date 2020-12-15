f761c31d154df9ce7029300c990cfaba2ab8e36f
Tony Watkins <tony@llifion.net>
2020-12-15 20:20:26 +0000

    Return an image for each item in the images array collection returned, using the map function. Note use of the key attribute - without this react throws a warning

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
-    return <div>Image List</div>
+    
+    const images = props.images.map((image) => {
+        // Assign a key to each item in a rendered collection - only required at the root component of each item
+        return <img src={image.urls.regular} key={image.id} alt={image.alt_description}/>
+    });
+    
+    return <div>{images}</div>
 };
 
 export default ImageList;
