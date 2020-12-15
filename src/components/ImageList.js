a776ac3107f211b680642b1b5124f6ab29979ee2
Tony Watkins <tony@llifion.net>
2020-12-15 20:26:40 +0000

    Use destructuring to assign individual variables for use in our generated image tags - cuts down on the noise and repeating image dot code

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ImageList = (props) => {
     
-    const images = props.images.map((image) => {
+    const images = props.images.map(({urls, id, alt_description}) => {
         // Assign a key to each item in a rendered collection - only required at the root component of each item
-        return <img src={image.urls.regular} key={image.id} alt={image.alt_description}/>
+        return <img src={urls.regular} key={id} alt={alt_description}/>
     });
     
     return <div>{images}</div>
