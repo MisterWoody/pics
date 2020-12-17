6dac782f6ae93d1cded95f6316429c306aedc3a7
Tony Watkins <tony@llifion.net>
2020-12-17 20:33:58 +0000

    Introduce a new image card component that we'll use to balance the image with the right amount of container space

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     
-    const images = props.images.map(({urls, id, alt_description}) => {
+    const images = props.images.map((image) => {
         // Assign a key to each item in a rendered collection - only required at the root component of each item
-        return <img src={urls.regular} key={id} alt={alt_description}/>
+        return <ImageCard key={image.id} image={image} />;
     });
     
     return <div className="image-list">{images}</div>
